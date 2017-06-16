24ebe61f56aeb2a930cc96d1f7a5cbec81d57cf9
Radik Chernyshov <radik.chernyshov@gmail.com>
2017-06-16 14:52:36 +0300

    Add environments examples

--- nodejs/ecmascript5/parcel.js
+++ nodejs/ecmascript5/parcel.js
@@ -1,172 +1,147 @@
-"use strict";
+'use strict';
 
-var dataUtils = require("../../../utilities/dataUtils");
+var dataUtils = require('../../../utilities/dataUtils');
 
-var defaultToNull = function (value) {
-    return value || null;
+var defaultToNull = function(value) {
+   return value || null;
 };
 
-var defaultToEmpty = function (value) {
-    return value || "";
+var defaultToEmpty = function(value) {
+   return value || '';
 };
 
-var _mapParcelToSql = function (parcel) {
-    return {
-        id: defaultToNull(parcel.id),
-        consignment_id: defaultToNull(parcel.consignmentId),
-        parcel_number: defaultToNull(parcel.parcelNumber),
-        parcelnet_label_number: defaultToNull(parcel.parcelnetLabelNumber),
-        is_voided: dataUtils.setBoolean(parcel.isVoided, false),
-        barcode: defaultToNull(parcel.barcode),
-        parcelnet_barcode: defaultToNull(parcel.parcelnetBarcode),
-        parcelnet_data: defaultToNull(parcel.parcelnetData),
-        created_date: parcel.createdDate || new Date(),
-        updated_date: defaultToNull(parcel.updatedDate),
-        customer_ref1: defaultToEmpty(parcel.customerRef1),
-        customer_ref2: defaultToEmpty(parcel.customerRef2),
-        customer_ref3: defaultToEmpty(parcel.customerRef3),
-        customer_ref4: defaultToEmpty(parcel.customerRef4)
-    };
+var _mapExampleToSql = function(example) {
+   return {
+      id: defaultToNull(example.id),
+      example_connected_id: defaultToNull(example.example_connectedId),
+      example_number: defaultToNull(example.exampleNumber),
+      examplenet_label_number: defaultToNull(example.examplenetLabelNumber),
+      is_voided: dataUtils.setBoolean(example.isVoided, false),
+      barcode: defaultToNull(example.barcode),
+      examplenet_barcode: defaultToNull(example.examplenetBarcode),
+      examplenet_data: defaultToNull(example.examplenetData),
+      created_date: example.createdDate || new Date(),
+      updated_date: defaultToNull(example.updatedDate),
+      customer_ref1: defaultToEmpty(example.customerRef1),
+      customer_ref2: defaultToEmpty(example.customerRef2),
+      customer_ref3: defaultToEmpty(example.customerRef3),
+      customer_ref4: defaultToEmpty(example.customerRef4),
+   };
 };
 
-// esgold: queryShipmentByConsolidationCriteria
-// esgold: queryShipmentByIdAccountIdUserId
-var getParcelByConsignmentId = function (mysql, params, callback) {
-
-    var values = [
-        params.id
-    ];
-
-    var sql = "SELECT * FROM parcel WHERE consignment_id = ?";
-
-    mysql.query(sql, values, function (err, data) {
-
-        callback(err, data);
-    });
+var getexampleByExampleConnectedId = function(mysql, params, callback) {
+   var values = [params.id];
+   var sql = 'SELECT * FROM example WHERE example_connected_id = ?';
+   mysql.query(sql, values, function(err, data) {
+      callback(err, data);
+   });
 };
 
-// esgold: updateShipmentById
-var insertParcel = function (mysql, params, callback) {
-
-    var values = [
-
-        _mapParcelToSql(params.parcel)
-
-    ];
-
-    var sql = "INSERT INTO parcel SET ?";
-
-
-    mysql.query(sql, values, function (err, result) {
-        return callback(err, result ? result : null);
-    });
+var insertExample = function(mysql, params, callback) {
+   var values = [_mapExampleToSql(params.example)];
+   var sql = 'INSERT INTO example SET ?';
+   mysql.query(sql, values, function(err, result) {
+      return callback(err, result ? result : null);
+   });
 
 };
 
-// esgold: updateShipmentById
-var updateParcelById = function (mysql, params, callback) {
-
-    var parcel = params.parcel;
-
-    if (params.toSql) {
-        parcel = _mapParcelToSql(params.parcel);
-    }
-
-    var values = [
-        parcel,
-        params.parcel.id
-    ];
-
-    var sql = "UPDATE parcel SET ?, updated_date = UTC_TIMESTAMP() WHERE id = ?";
-
-    mysql.query(sql, values, callback);
+var updateExampleById = function(mysql, params, callback) {
+   var example = params.example;
+   if (params.toSql) {
+      example = _mapExampleToSql(params.example);
+   }
+   var values = [
+      example,
+      params.example.id,
+   ];
+   var sql = 'UPDATE example SET ?, updated_date = UTC_TIMESTAMP() WHERE id = ?';
+   mysql.query(sql, values, callback);
 };
 
-// esgold: identifyShipment
-var getDpdParcelNumber = function (mysql, params, callback) {
-    var sql = "select next_parcel_value() as value";
-
-    mysql.query(sql, [], callback);
+var getDpdexampleNumber = function(mysql, params, callback) {
+   var sql = 'select next_example_value() as value';
+   mysql.query(sql, [], callback);
 };
 
-var voidParcelsByShipmentId = function (mysql, params, callback) {
-
-    var sql = [
-        "UPDATE parcel",
-        "INNER JOIN consignment ON parcel.consignment_id = consignment.id",
-        "INNER JOIN shipment ON consignment.shipment_id = shipment.id",
-        "SET parcel.is_voided = ?, parcel.updated_date = CURRENT_TIMESTAMP(), parcel.void_status_date = CURRENT_TIMESTAMP()",
-        "WHERE shipment.id = ?"
-    ].join(" ");
-
-    var values = [params.isVoided, params.id];
-
-    mysql.query(sql, values, callback);
+var voidExamplesBySomeId = function(mysql, params, callback) {
+   var sql = [
+      'UPDATE example',
+      'INNER JOIN example_connected ON example.example_connected_id = example_connected.id',
+      'INNER JOIN some ON example_connected.some_id = some.id',
+      'SET example.is_voided = ?, example.updated_date = CURRENT_TIMESTAMP(), example.void_status_date = CURRENT_TIMESTAMP()',
+      'WHERE some.id = ?',
+   ].join(' ');
+   var values = [params.isVoided, params.id];
+   mysql.query(sql, values, callback);
 };
 
-var voidParcelsByShipmentIdAndNumber = function (mysql, params, callback) {
-    var sqlQuery = [
-        "UPDATE parcel",
-        "INNER JOIN consignment ON parcel.consignment_id = consignment.id",
-        "INNER JOIN shipment ON consignment.shipment_id = shipment.id",
-        "SET parcel.is_voided = ?, parcel.updated_date = CURRENT_TIMESTAMP(), parcel.void_status_date = CURRENT_TIMESTAMP()",
-        "WHERE shipment.id = ? AND parcel.parcel_number IN (?)"
-    ].join(" ");
-    mysql.query(sqlQuery, [params.isVoided, params.id, params.numbers], callback);
+var voidexamplesBySomeIdAndNumber = function(mysql, params, callback) {
+   var sqlQuery = [
+      'UPDATE example',
+      'INNER JOIN example_connected ON example.example_connected_id = example_connected.id',
+      'INNER JOIN some ON example_connected.some_id = some.id',
+      'SET example.is_voided = ?, example.updated_date = CURRENT_TIMESTAMP(), example.void_status_date = CURRENT_TIMESTAMP()',
+      'WHERE some.id = ? AND example.example_number IN (?)',
+   ].join(' ');
+   mysql.query(sqlQuery, [params.isVoided, params.id, params.numbers], callback);
 };
 
-var getShipmentParcels = function (shipmentId, mysql, callback) {
-    var sqlQuery = [
-        "SELECT p.* FROM parcel p",
-        "INNER JOIN consignment c ON p.consignment_id = c.id",
-        "INNER JOIN shipment s ON c.shipment_id = s.id",
-        "WHERE s.id = ?"
-    ].join(" ");
-    mysql.query(sqlQuery, [shipmentId], function (err, data) {
-        if (err) {
-            callback(err);
-        }
-        callback(null, data);
-    });
+var getSomeexamples = function(someId, mysql, callback) {
+   var sqlQuery = [
+      'SELECT p.* FROM example p',
+      'INNER JOIN example_connected c ON p.example_connected_id = c.id',
+      'INNER JOIN some s ON c.some_id = s.id',
+      'WHERE s.id = ?',
+   ].join(' ');
+   mysql.query(sqlQuery, [someId], function(err, data) {
+      if (err) {
+         callback(err);
+      }
+      callback(null, data);
+   });
 };
 
-var getShipmentConsignment = function (shipmentId, mysql, callback) {
-    var sqlQuery = [
-        "SELECT consignment.*, shipment.is_voided FROM consignment",
-        "INNER JOIN shipment ON consignment.shipment_id = shipment.id",
-        "WHERE shipment.id = ? LIMIT 1"
-    ].join(" ");
-    mysql.query(sqlQuery, [shipmentId], function (err, result) {
-        if (err) {
-            return callback(err);
-        }
-        callback(null, result[0]);
-    });
+var getSomeExampleConnected = function(someId, mysql, callback) {
+   var sqlQuery = [
+      'SELECT example_connected.*, some.is_voided FROM example_connected',
+      'INNER JOIN some ON example_connected.some_id = some.id',
+      'WHERE some.id = ? LIMIT 1',
+   ].join(' ');
+   mysql.query(sqlQuery, [someId], function(err, result) {
+      if (err) {
+         return callback(err);
+      }
+      callback(null, result[0]);
+   });
 };
 
-var updateShipmentConsignment = function (consignmentId, consignmentWeight, parcelsCount, mysql, callback) {
-
-    var sqlQuery = [
-        "UPDATE consignment",
-        "SET total_weight = ?, updated_date = CURRENT_TIMESTAMP(), number_of_parcels = (SELECT COUNT(id) FROM parcel WHERE parcel.consignment_id = ? AND is_voided = FALSE)",
-        "WHERE id = ?"
-    ].join(" ");
-    mysql.query(sqlQuery, [consignmentWeight, consignmentId, consignmentId], function (err) {
-        if (err) {
-            return callback(err);
-        }
-        callback(null);
-    });
+var updateSomeExampleConnected = function(
+    example_connectedId, example_connectedWeight, examplesCount, mysql,
+    callback) {
+   var sqlQuery = [
+      'UPDATE example_connected',
+      'SET total_weight = ?, updated_date = CURRENT_TIMESTAMP(), number_of_examples = (SELECT COUNT(id) FROM example WHERE example.example_connected_id = ? AND is_voided = FALSE)',
+      'WHERE id = ?',
+   ].join(' ');
+   mysql.query(sqlQuery,
+       [example_connectedWeight, example_connectedId, example_connectedId],
+       function(err) {
+          if (err) {
+             return callback(err);
+          }
+          callback(null);
+       });
 };
 
 module.exports = {
-    getParcelByConsignmentId: getParcelByConsignmentId,
-    insertParcel: insertParcel,
-    updateParcelById: updateParcelById,
-    getDpdParcelNumber: getDpdParcelNumber,
-    voidParcelsByShipmentId: voidParcelsByShipmentId,
-    voidParcelsByShipmentIdAndNumber: voidParcelsByShipmentIdAndNumber,
-    getShipmentParcels: getShipmentParcels,
-    getShipmentConsignment: getShipmentConsignment,
-    updateShipmentConsignment: updateShipmentConsignment
+   getexEmpleByexampleConnectedId: getexampleByExampleConnectedId,
+   insertExample: insertExample,
+   updateexampleById: updateExampleById,
+   getDpdExampleNumber: getDpdexampleNumber,
+   voidExamplesBySomeId: voidExamplesBySomeId,
+   voidExamplesBySomeIdAndNumber: voidexamplesBySomeIdAndNumber,
+   getSomeExamples: getSomeexamples,
+   getSomeExampleConnected: getSomeExampleConnected,
+   updateSomeExampleConnected: updateSomeExampleConnected,
 };
--- nodejs/ecmascript5/sqlUtils.js
+++ nodejs/ecmascript5/sqlUtils.js
@@ -1,124 +1,123 @@
-"use strict";
+'use strict';
 
-var _ = require("underscore");
+var _ = require('underscore');
 
 function _connectionFromPool($pool, fn, callback) {
-    $pool.getConnection(function (err, connection) {
-        if (err)
+   $pool.getConnection(function(err, connection) {
+      if (err) {
+         return callback(err);
+      }
+      fn(connection, function(err, result) {
+         connection.release();
+         if (err) {
             return callback(err);
-
-        fn(connection, function (err, result) {
-            connection.release();
-            if (err) {
-                return callback(err);
-            }
-            callback(null, result);
-        });
-
-    });
-
+         }
+         callback(null, result);
+      });
+   });
 }
 
 function _withinTransaction(connection, fn, callback) {
-    connection.beginTransaction(function (err) {
-        if (err) {
+   connection.beginTransaction(function(err) {
+      if (err) {
+         return callback(err);
+      }
+      fn(connection, function(err, result) {
+         if (err) {
+            connection.rollback();
             return callback(err);
-        }
-        fn(connection, function (err, result) {
+         }
+         connection.commit(function(err) {
             if (err) {
-                connection.rollback();
-                return callback(err);
+               connection.rollback();
+               return callback(err);
             }
-            connection.commit(function (err) {
-                if (err) {
-                    connection.rollback();
-                    return callback(err);
-                }
-                callback(null, result);
-            });
-        });
-    });
+            callback(null, result);
+         });
+      });
+   });
 }
 
 function usingTransactionFromPool(connectionPool, fn, callback) {
-    _connectionFromPool(connectionPool, function (connection, releaseFn) {
-        _withinTransaction(connection, fn, releaseFn);
-    }, callback);
+   _connectionFromPool(connectionPool, function(connection, releaseFn) {
+      _withinTransaction(connection, fn, releaseFn);
+   }, callback);
 }
 
 function generateInClause(size) {
-    var sql = "in (";
-    for (var i = 0; i < size; i++) {
-        sql += "?";
-        if (i < size - 1) {
-            sql += ", ";
-        }
-    }
-    sql += ")";
-    return sql;
+   var sql = 'in (';
+   for (var i = 0; i < size; i++) {
+      sql += '?';
+      if (i < size - 1) {
+         sql += ', ';
+      }
+   }
+   sql += ')';
+   return sql;
 }
 
 function getDateInCorrectFormat(date) {
-    return "STR_TO_DATE(\'" + date.toISOString() + "\', \'%Y-%m-%dT%H:%i:%s\')";
+   return 'STR_TO_DATE(\'' + date.toISOString() + '\', \'%Y-%m-%dT%H:%i:%s\')';
 }
 
 function generateBetweenClause(value) {
-    var startDate = "";
-    var endDate = "";
-    if (value instanceof Date) {
-        startDate = new Date(value.getTime());
-        endDate = new Date(value.getTime());
-    }
-    if (value instanceof Array) {
-        startDate = value[0];
-        endDate = value[1];
-    }
-    if (startDate !== "" && endDate !== "") {
-        startDate.setHours(0, 0, 0, 0);
-        endDate.setHours(23, 59, 59, 999);
-        return "BETWEEN " + getDateInCorrectFormat(startDate) + " AND " + getDateInCorrectFormat(endDate);
-    }
-    return "";
+   var startDate = '';
+   var endDate = '';
+   if (value instanceof Date) {
+      startDate = new Date(value.getTime());
+      endDate = new Date(value.getTime());
+   }
+   if (value instanceof Array) {
+      startDate = value[0];
+      endDate = value[1];
+   }
+   if (startDate !== '' && endDate !== '') {
+      startDate.setHours(0, 0, 0, 0);
+      endDate.setHours(23, 59, 59, 999);
+      return 'BETWEEN ' + getDateInCorrectFormat(startDate) + ' AND ' +
+          getDateInCorrectFormat(endDate);
+   }
+   return '';
 }
 
 function generateWhereClauseFromFieldList(fieldList) {
-    if (fieldList.length === 0) {
-        return "";
-    }
-    var clause = " WHERE ";
-    for (var i = 0; i < fieldList.length - 1; i++) {
-        var field = fieldList[i];
-        clause += field + " = " + "? AND ";
-    }
-    clause += fieldList[fieldList.length - 1] + " = ?";
-    return clause;
+   if (fieldList.length === 0) {
+      return '';
+   }
+   var clause = ' WHERE ';
+   for (var i = 0; i < fieldList.length - 1; i++) {
+      var field = fieldList[i];
+      clause += field + ' = ' + '? AND ';
+   }
+   clause += fieldList[fieldList.length - 1] + ' = ?';
+   return clause;
 }
 
 function reduceFieldListArrayWithPrefix(fieldList, prefix) {
-    if (!Array.isArray(fieldList)) {
-        return "";
-    }
-    var fieldListLastIndex = fieldList.length - 1;
-    return fieldList.reduce(function (fieldString, field, index) {
-        fieldString += prefix + field;
-        if (index !== fieldListLastIndex) {
-            fieldString += ", ";
-        }
-        return fieldString;
-    }, "");
+   if (!Array.isArray(fieldList)) {
+      return '';
+   }
+   var fieldListLastIndex = fieldList.length - 1;
+   return fieldList.reduce(function(fieldString, field, index) {
+      fieldString += prefix + field;
+      if (index !== fieldListLastIndex) {
+         fieldString += ', ';
+      }
+      return fieldString;
+   }, '');
 }
 
 function getUpdateValues(columns) {
-    return _.map(columns, function (value) {
-        return "`" + value + "` = VALUES(`" + value + "`)";
-    });
+   return _.map(columns, function(value) {
+      return '`' + value + '` = VALUES(`' + value + '`)';
+   });
 }
 
 module.exports = {
-    usingTransactionFromPool: usingTransactionFromPool,
-    generateInClause: generateInClause,
-    generateBetweenClause: generateBetweenClause,
-    generateWhereClauseFromFieldList: generateWhereClauseFromFieldList,
-    reduceFieldListArrayWithPrefix: reduceFieldListArrayWithPrefix,
-    getUpdateValues: getUpdateValues
+   usingTransactionFromPool: usingTransactionFromPool,
+   generateInClause: generateInClause,
+   generateBetweenClause: generateBetweenClause,
+   generateWhereClauseFromFieldList: generateWhereClauseFromFieldList,
+   reduceFieldListArrayWithPrefix: reduceFieldListArrayWithPrefix,
+   getUpdateValues: getUpdateValues,
 };
