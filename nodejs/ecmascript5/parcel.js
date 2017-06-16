cd64aace4c74ab2cabb0bb5fc4a67934fd4f770c
Radik Chernyshov <radik.chernyshov@gmail.com>
2017-06-16 14:57:14 +0300

    Add environments examples

--- nodejs/ecmascript5/parcel.js
+++ nodejs/ecmascript5/parcel.js
@@ -29,24 +29,24 @@ var _mapExampleToSql = function(example) {
    };
 };
 
-var getexampleByExampleConnectedId = function(mysql, params, callback) {
+var getExampleByExampleConnectedId = function(connection, params, callback) {
    var values = [params.id];
    var sql = 'SELECT * FROM example WHERE example_connected_id = ?';
-   mysql.query(sql, values, function(err, data) {
+   connection.query(sql, values, function(err, data) {
       callback(err, data);
    });
 };
 
-var insertExample = function(mysql, params, callback) {
+var insertExample = function(connection, params, callback) {
    var values = [_mapExampleToSql(params.example)];
    var sql = 'INSERT INTO example SET ?';
-   mysql.query(sql, values, function(err, result) {
+   connection.query(sql, values, function(err, result) {
       return callback(err, result ? result : null);
    });
 
 };
 
-var updateExampleById = function(mysql, params, callback) {
+var updateExampleById = function(connection, params, callback) {
    var example = params.example;
    if (params.toSql) {
       example = _mapExampleToSql(params.example);
@@ -56,15 +56,15 @@ var updateExampleById = function(mysql, params, callback) {
       params.example.id,
    ];
    var sql = 'UPDATE example SET ?, updated_date = UTC_TIMESTAMP() WHERE id = ?';
-   mysql.query(sql, values, callback);
+   connection.query(sql, values, callback);
 };
 
-var getDpdexampleNumber = function(mysql, params, callback) {
+var getExampleNumber = function(connection, params, callback) {
    var sql = 'select next_example_value() as value';
-   mysql.query(sql, [], callback);
+   connection.query(sql, [], callback);
 };
 
-var voidExamplesBySomeId = function(mysql, params, callback) {
+var voidExamplesBySomeId = function(connection, params, callback) {
    var sql = [
       'UPDATE example',
       'INNER JOIN example_connected ON example.example_connected_id = example_connected.id',
@@ -73,10 +73,10 @@ var voidExamplesBySomeId = function(mysql, params, callback) {
       'WHERE some.id = ?',
    ].join(' ');
    var values = [params.isVoided, params.id];
-   mysql.query(sql, values, callback);
+   connection.query(sql, values, callback);
 };
 
-var voidexamplesBySomeIdAndNumber = function(mysql, params, callback) {
+var voidExamplesBySomeIdAndNumber = function(connection, params, callback) {
    var sqlQuery = [
       'UPDATE example',
       'INNER JOIN example_connected ON example.example_connected_id = example_connected.id',
@@ -84,17 +84,17 @@ var voidexamplesBySomeIdAndNumber = function(mysql, params, callback) {
       'SET example.is_voided = ?, example.updated_date = CURRENT_TIMESTAMP(), example.void_status_date = CURRENT_TIMESTAMP()',
       'WHERE some.id = ? AND example.example_number IN (?)',
    ].join(' ');
-   mysql.query(sqlQuery, [params.isVoided, params.id, params.numbers], callback);
+   connection.query(sqlQuery, [params.isVoided, params.id, params.numbers], callback);
 };
 
-var getSomeexamples = function(someId, mysql, callback) {
+var getSomeExamples = function(someId, connection, callback) {
    var sqlQuery = [
       'SELECT p.* FROM example p',
       'INNER JOIN example_connected c ON p.example_connected_id = c.id',
       'INNER JOIN some s ON c.some_id = s.id',
       'WHERE s.id = ?',
    ].join(' ');
-   mysql.query(sqlQuery, [someId], function(err, data) {
+   connection.query(sqlQuery, [someId], function(err, data) {
       if (err) {
          callback(err);
       }
@@ -102,13 +102,13 @@ var getSomeexamples = function(someId, mysql, callback) {
    });
 };
 
-var getSomeExampleConnected = function(someId, mysql, callback) {
+var getSomeExampleConnected = function(someId, connection, callback) {
    var sqlQuery = [
       'SELECT example_connected.*, some.is_voided FROM example_connected',
       'INNER JOIN some ON example_connected.some_id = some.id',
       'WHERE some.id = ? LIMIT 1',
    ].join(' ');
-   mysql.query(sqlQuery, [someId], function(err, result) {
+   connection.query(sqlQuery, [someId], function(err, result) {
       if (err) {
          return callback(err);
       }
@@ -117,15 +117,15 @@ var getSomeExampleConnected = function(someId, mysql, callback) {
 };
 
 var updateSomeExampleConnected = function(
-    example_connectedId, example_connectedWeight, examplesCount, mysql,
+    exampleConnectedId, exampleConnectedWeight, examplesCount, connection,
     callback) {
    var sqlQuery = [
       'UPDATE example_connected',
       'SET total_weight = ?, updated_date = CURRENT_TIMESTAMP(), number_of_examples = (SELECT COUNT(id) FROM example WHERE example.example_connected_id = ? AND is_voided = FALSE)',
       'WHERE id = ?',
    ].join(' ');
-   mysql.query(sqlQuery,
-       [example_connectedWeight, example_connectedId, example_connectedId],
+   connection.query(sqlQuery,
+       [exampleConnectedWeight, exampleConnectedId, exampleConnectedId],
        function(err) {
           if (err) {
              return callback(err);
@@ -135,13 +135,13 @@ var updateSomeExampleConnected = function(
 };
 
 module.exports = {
-   getexEmpleByexampleConnectedId: getexampleByExampleConnectedId,
+   getExmpleByexampleConnectedId: getExampleByExampleConnectedId,
    insertExample: insertExample,
    updateexampleById: updateExampleById,
-   getDpdExampleNumber: getDpdexampleNumber,
+   getDpdExampleNumber: getExampleNumber,
    voidExamplesBySomeId: voidExamplesBySomeId,
-   voidExamplesBySomeIdAndNumber: voidexamplesBySomeIdAndNumber,
-   getSomeExamples: getSomeexamples,
+   voidExamplesBySomeIdAndNumber: voidExamplesBySomeIdAndNumber,
+   getSomeExamples: getSomeExamples,
    getSomeExampleConnected: getSomeExampleConnected,
    updateSomeExampleConnected: updateSomeExampleConnected,
 };
